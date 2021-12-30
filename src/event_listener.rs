f60ebe4ad346b8bc7f6f07be29b8cd11de498db5
Gael Macherel <gael.macherel@gmail.com>
2021-12-30 19:39:58 +0100

    Define a new trait to allow creating objects with callbacks to react on new events

---
+++ src/event_listener.rs
@@ -0,0 +1,80 @@
+//!
+use crate::model::{
+    Ace, BaronKill, ChampionKill, DragonKill, Event, FirstBlood, FirstBrick, GameStart, HeraldKill,
+    InhibKilled, MinionsSpawning, Multikill, Team, TurretKilled,
+};
+use async_trait::async_trait;
+use std::error::Error;
+
+#[async_trait]
+pub trait EventListener: Send + Sync {
+    type Error: Error + Send + Sync;
+    async fn on_event(&mut self, event: Event) -> Result<(), Self::Error> {
+        match event {
+            Event::GameStart(event) => self.on_game_start(event).await,
+            Event::MinionsSpawning(event) => self.on_minion_spawn(event).await,
+            Event::FirstBrick(event) => self.on_first_brick(event).await,
+            Event::FirstBlood(event) => self.on_first_blood(event).await,
+            Event::TurretKilled(event) => self.on_turret_killed(event).await,
+            Event::InhibKilled(event) => self.on_inhib_killed(event).await,
+            Event::DragonKill(event) => self.on_dragon_killed(event).await,
+            Event::HeraldKill(event) => self.on_herald_killed(event).await,
+            Event::BaronKill(event) => self.on_baron_killed(event).await,
+            Event::ChampionKill(event) => self.on_champion_kill(event).await,
+            Event::Multikill(event) => self.on_multi_kill(event).await,
+            Event::Ace(event) => self.on_ace(event).await,
+        }
+    }
+
+    async fn on_game_start(&mut self, event: GameStart) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_minion_spawn(&mut self, event: MinionsSpawning) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_first_brick(&mut self, event: FirstBrick) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_first_blood(&mut self, event: FirstBlood) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_turret_killed(&mut self, event: TurretKilled) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_inhib_killed(&mut self, event: InhibKilled) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_dragon_killed(&mut self, event: DragonKill) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_herald_killed(&mut self, event: HeraldKill) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_baron_killed(&mut self, event: BaronKill) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_champion_kill(&mut self, event: ChampionKill) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_multi_kill(&mut self, event: Multikill) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_ace(&mut self, event: Ace) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn on_error(&mut self, error: Self::Error) {
+        log::error!("{}", error)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,56 @@
-pub mod model;
+use crate::api::GameClient;
+use crate::event_listener::EventListener;
+use std::error::Error;
+use std::time::Duration;
+
 pub mod api;
+pub mod event_listener;
+pub mod model;
+
+pub extern crate async_trait; // Re-export async_trait
+
+/// Take an event listener, and start a tasks that will periodically list all new events, and
+/// pass them to the listener callbacks.
+///
+/// This default implementation is fairly naive, and may quickly be obsolete.
+/// Not only does it require a strong bound on the Error type, but it also has poor performances.
+/// This waits for an event to be fully processed before processing another one. If a processing take some time,
+/// the processing of future events will be strongly delayed, which is suboptimal.
+pub fn start_listener<L: EventListener + 'static>(mut listener: L, period: Duration) {
+    tokio::spawn(async move {
+        let client = GameClient::new();
+        let mut previous_number_of_events = 0;
+        loop {
+            let events = match client.event_data().await {
+                Ok(events) => events.events,
+                Err(err) => {
+                    log::error!("Failed to get event data: {}", err);
+                    tokio::time::sleep(period).await;
+                    continue;
+                }
+            };
+
+            let number_of_events = events.len();
+
+            if number_of_events <= previous_number_of_events {
+                // Nothing new...
+                log::debug!("Nothing new, still {} events", number_of_events);
+                tokio::time::sleep(period).await;
+                continue;
+            }
+
+            for event in events.as_slice()[previous_number_of_events..]
+                .to_vec()
+                .into_iter()
+            {
+                match listener.on_event(event).await {
+                    Ok(_) => {}
+                    Err(err) => listener.on_error(err).await,
+                };
+            }
+
+            previous_number_of_events = number_of_events;
+            tokio::time::sleep(period).await;
+        }
+    });
+}
